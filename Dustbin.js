1f2f30a9bc6d79e11fc3e9122789fb604fd2efe0
Kanishkaarora7 <62178929+Kanishkaarora7@users.noreply.github.com>
2020-05-30 19:31:02 +0530

    Add files via upload

---
+++ Dustbin.js
@@ -0,0 +1,14 @@
+class Dustbin extends Base {
+    constructor(x,y,width,height){
+       
+    
+       super(x,y,width,height)
+       
+       
+    
+    }
+    
+   display(){
+    super.Basedisplay()  
+} 
+}
---
+++ Trash.js
@@ -0,0 +1,17 @@
+class Trash extends Base {
+    constructor(x,y){
+       
+    
+       super(x,y,40,40)
+       
+       
+       
+    
+    }
+    
+   display(){
+       //this.body.position.x=mouseX
+      // this.body.position.y=mouseY
+      super.Basedisplay()
+} 
+}
